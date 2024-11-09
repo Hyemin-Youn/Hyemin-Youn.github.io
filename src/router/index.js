9341e80c2d698d7563db31e083694448e5dfad1d
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-09 22:57:16 +0900

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 // src/router/index.js
+
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import MovieList from '@/components/MovieList.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
+  { path: '/movies', component: MovieList },
 ];
 
 const router = createRouter({

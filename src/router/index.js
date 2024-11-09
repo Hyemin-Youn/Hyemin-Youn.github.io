4b0c383f83aafb291a467faeea20d51f8fe2b983
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-09 19:23:19 +0900

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/Home.vue"; // 경로를 실제 컴포넌트 위치에 맞게 조정하세요.
-import UserLogin from "../UserLogin.vue";
+import UserLogin from "../components/UserLogin.vue";
 
 const routes = [
   { path: "/", component: UserLogin },

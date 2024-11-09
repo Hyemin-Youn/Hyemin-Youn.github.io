3cd74e23eba094c4f58916442a8a0c4390a435e1
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-09 23:31:36 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
-// .eslintrc.js
 module.exports = {
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module"
+    },
+    parser: "babel-eslint",
     rules: {
       'vue/multi-word-component-names': 'off', // 이 규칙 비활성화
     },

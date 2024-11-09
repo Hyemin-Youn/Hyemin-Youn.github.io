fa7b4720ce8070b5c763e39284bd092a961ae086
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-09 21:19:56 +0900

    update

--- src/URL.js
+++ src/URL.js
@@ -1,14 +1,12 @@
-import axios from 'axios';
-
-const apiKey = 'apikey'; // 실제 API 키로 변경하세요
+const apiKey = process.env.VUE_APP_API_KEY; // 환경 변수에서 API 키 가져오기
 const language = 'ko-KR';
 
-// API 엔드포인트 구성
 const endpoints = {
   popularMovies: (page = 1) => `/movie/popular?api_key=${apiKey}&language=${language}&page=${page}`,
   nowPlayingMovies: (page = 1) => `/movie/now_playing?api_key=${apiKey}&language=${language}&page=${page}`,
   discoverMovies: (page = 1) => `/discover/movie?api_key=${apiKey}&language=${language}&page=${page}`,
 };
+
 
 // 데이터 요청 처리 함수
 export const fetchMovies = async (url) => {

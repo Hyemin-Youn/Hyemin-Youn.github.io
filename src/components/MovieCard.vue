36bb39f6e2c04368e5e4947bca1c99aa9163073b
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-11 00:16:17 +0900

    update

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,23 @@
+<template> 
+    <div class="movie-card" @click="goToDetail"> 
+      <img :src="movie.poster" alt="Movie Poster" /> 
+      <h3>{{ movie.title }}</h3> 
+    </div> 
+  </template> 
+   
+  <script> 
+  export default { 
+    props: ['movie'], 
+    methods: { 
+      goToDetail() { 
+        this.$router.push(`/movie/${this.movie.id}`); 
+      }, 
+    }, 
+  }; 
+  </script> 
+   
+  <style scoped> 
+  .movie-card { 
+    cursor: pointer; 
+  } 
+  </style> 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template> 
+    <nav> 
+      <h1>Netflix Clone</h1> 
+      <router-link to="/">Home</router-link> 
+      <router-link to="/login">Login</router-link> 
+    </nav> 
+  </template> 
+   
+  <script> 
+  export default { 
+    name: 'Navbar', 
+  }; 
+  </script> 

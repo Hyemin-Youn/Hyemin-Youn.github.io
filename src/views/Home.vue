18ef233f92f3ec968312ac60110ea2e477621fcb
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-11 05:17:29 +0900

    update api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -55,7 +55,7 @@
     },
     methods: {
       async fetchHeroMovie() {
-        const API_KEY = '75b7c972001662bd9d37622e0e222947';
+        const API_KEY = process.env.VUE_VUE_APP_API_KEY;
         try {
           const response = await axios.get(
             `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR`
@@ -66,7 +66,7 @@
         }
       },
       async fetchMovies() {
-        const API_KEY = '75b7c972001662bd9d37622e0e222947';
+        const API_KEY = process.env.VUE_VUE_APP_API_KEY;
         const requests = this.movieCategories.map(async (category) => {
           const response = await axios.get(
             `https://api.themoviedb.org/3/movie/${category.name}?api_key=${API_KEY}&language=ko-KR`

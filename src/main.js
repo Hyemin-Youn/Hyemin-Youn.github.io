0c0d5e8b73472080e28b8e905ad22f157a47db5e
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-11 05:47:29 +0900

    update

--- src/main.js
+++ src/main.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-import { createApp } from 'vue'
-import App from './App.vue'
-=======
 // src/main.js
 import { createApp } from 'vue';
 import App from './App.vue';
->>>>>>> feature/Home
 import router from './router'; // router 설정 가져오기
 import '@fortawesome/fontawesome-free/css/all.css';
 
 const app = createApp(App);
 app.use(router);
-<<<<<<< HEAD
 app.mount('#app');
-=======
-app.mount('#app');
->>>>>>> feature/Home

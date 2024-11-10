61849278b4c00ac23ce70659445ef5570b3a2fe9
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-10 10:11:03 +0900

    update Home.vue

---
+++ src/components/home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+      <Header />
+      <div id="container">
+        <router-view></router-view>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Header from '@/layout/header/Header.vue';
+  
+  export default {
+    components: {
+      Header
+    }
+  };
+  </script>
+  
+  <style scoped>
+  #app {
+    width: 95vw;
+    height: 95vh;
+    overflow-x: hidden;
+    margin: auto;
+  }
+  </style>
+  

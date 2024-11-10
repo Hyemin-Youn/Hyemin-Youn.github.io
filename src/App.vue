68aad38ead20c28a6ab90f3f82515db1121fbbb2
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-11 02:55:54 +0900

    update Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div id="app">
     <router-view />
-    <Footer />
   </div>
 </template>
 
 <script>
-import Footer from './components/Footer.vue';
+
 
 
 
 /* eslint-disable */
 export default {
   components: {
-    Navbar,
-    Footer
+
   }
 };
 

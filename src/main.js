711fe154af6516d28926d2c2f43e3ead7a5d4b68
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-13 11:14:58 +0900

    update deploy.yml

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // router 설정 가져오기
+import store from './store';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount('#app');

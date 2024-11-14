a20a9ffcb2465ee5081853eb1b9a24edcaac00bd
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-15 04:15:42 +0900

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/https://hyemin-youn.github.io//'
+    ? '/hyemin-youn.github.io/'
     : '/'
 }

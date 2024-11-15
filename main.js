c8654f4fe3e123a7e5820d9c48bbf154723864b2
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-15 12:04:29 +0900

    update main.js and style.css

---
+++ main.js
@@ -0,0 +1,11 @@
+// src/main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // router 설정 가져오기
+import store from './store';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount('#app');

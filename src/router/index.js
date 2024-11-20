a35c94a7cd94d4deb37f55fc2b5ff5260e9cf911
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-20 14:17:50 +0900

    update router home

--- src/router/index.js
+++ src/router/index.js
@@ -31,11 +31,8 @@ router.beforeEach((to, from, next) => {
 
   console.log('From:', from.path, 'To:', to.path, 'isAuthenticated:', isAuthenticated);
 
-  if (to.meta.requiresAuth && !isAuthenticated) {
-    // 인증이 필요한 경로인데 인증되지 않은 경우
-    console.log('Redirecting to /signin');
-    next('/signin');
-  } else if (to.path === '/signin' && isAuthenticated) {
+  // 
+  if (to.path === '/signin' && isAuthenticated) {
     // 인증된 사용자가 /signin으로 이동하려 할 때
     console.log('Redirecting to /home');
     next('/home');

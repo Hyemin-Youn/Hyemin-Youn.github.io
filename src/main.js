e2b03c2a017713753b39b37ce6edab8b653538d6
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-20 11:18:23 +0900

    delete router in main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 // src/main.js
-import { createRouter, createWebHistory } from 'vue-router';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // router 설정 가져오기
 import store from './store';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-  });
-  
+
 const app = createApp(App);
 app.use(router);
 app.use(store);

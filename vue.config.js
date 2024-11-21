9890b3c3bb93519ca5c23328599d07151a56dc63
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-21 16:36:05 +0900

    update MovieDatils

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
 })
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Hyemin-Youn.github.io/' : './'
+  publicPath: process.env.NODE_ENV === 'production' ? '/Hyemin-Youn.github.io/' : '/'
   //publicPath: process.env.NODE_ENV === 'production' ? '/이곳에 경로를 적으시오./' : '/'
 };

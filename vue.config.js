9420eb15128a537b0373a13086c1bce6a2281d0a
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-24 13:30:55 +0900

    update API /popular

--- vue.config.js
+++ vue.config.js
@@ -7,3 +7,6 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : './'
   //publicPath: process.env.NODE_ENV === 'production' ? '/이곳에 경로를 적으시오./' : '/'
 };
+
+export const API_KEY = 'your_tmdb_api_key';
+export const BASE_URL = 'https://api.themoviedb.org/3';

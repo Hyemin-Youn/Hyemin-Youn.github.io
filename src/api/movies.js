aadeaf7354493521188dfe6e1ea2f69ad2d991d5
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-24 16:02:09 +0900

    update Search

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,25 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
+import { API_KEY, BASE_URL } from '@/config';
 
-const API_KEY = "your_tmdb_api_key"; // 여기에 TMDb API 키를 입력하세요
-const BASE_URL = "https://api.themoviedb.org/3";
-
-export const fetchMovies = async (filters) => {
-  const { genre, rating, language } = filters;
-
+export const fetchPopularMovies = async (page = 1) => {
   try {
-    const response = await axios.get(`${BASE_URL}/discover/movie`, {
+    const response = await axios.get(`${BASE_URL}/movie/popular`, {
       params: {
         api_key: API_KEY,
-        language: "ko-KR",
-        with_genres: genre !== "장르 (전체)" ? genre : null,
-        "vote_average.gte": rating !== "평점 (전체)" ? parseFloat(rating.split("~")[0]) : null,
-        "vote_average.lte": rating !== "평점 (전체)" ? parseFloat(rating.split("~")[1]) : null,
-        with_original_language: language !== "언어 (전체)" ? language : null,
+        language: 'ko-KR',
+        page,
       },
     });
-    return response.data.results;
+    return response.data;
   } catch (error) {
-    console.error("Error fetching movies:", error);
-    return [];
+    console.error('Error fetching popular movies:', error);
+    return { results: [] };
   }
 };

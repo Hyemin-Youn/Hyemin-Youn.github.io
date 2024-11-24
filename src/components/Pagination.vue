cd453685a058ed20dfba9028e8c8fb8679c01f64
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-24 18:43:48 +0900

    update search

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,60 +1,44 @@
 <template>
-  <div class="pagination">
-    <button :disabled="currentPage === 1" @click="changePage(currentPage - 1)">
-      이전
-    </button>
-    <span>{{ currentPage }} / {{ totalPages }}</span>
-    <button :disabled="currentPage === totalPages" @click="changePage(currentPage + 1)">
-      다음
-    </button>
-  </div>
-</template>
-
-<script>
-export default {
-  props: {
-    currentPage: {
-      type: Number,
-      required: true,
+    <div class="pagination">
+      <button @click="prevPage" :disabled="currentPage === 1">이전</button>
+      <span>{{ currentPage }} / {{ totalPages }}</span>
+      <button @click="nextPage" :disabled="currentPage === totalPages">다음</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      currentPage: {
+        type: Number,
+        required: true,
+      },
+      totalPages: {
+        type: Number,
+        required: true,
+      },
     },
-    totalPages: {
-      type: Number,
-      required: true,
+    methods: {
+      prevPage() {
+        this.$emit('change-page', this.currentPage - 1);
+      },
+      nextPage() {
+        this.$emit('change-page', this.currentPage + 1);
+      },
     },
-  },
-  methods: {
-    changePage(page) {
-      this.$emit("change-page", page);
-    },
-  },
-};
-</script>
-
-<style scoped>
-.pagination {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  margin-top: 20px;
-}
-
-button {
-  padding: 5px 10px;
-  background-color: #e50914;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  transition: background-color 0.3s;
-}
-
-button:disabled {
-  background-color: #555;
-  cursor: not-allowed;
-}
-
-span {
-  color: white;
-}
-</style>
+  };
+  </script>
+  
+  <style scoped>
+  .pagination {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    margin-top: 20px;
+  }
+  button {
+    padding: 5px 10px;
+  }
+  </style>
+  

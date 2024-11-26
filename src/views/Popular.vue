3a04b599245430fd5039032e55533667ff5d6fb7
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-26 13:19:07 +0900

    update Popular.vue

--- src/views/Popular.vue
+++ src/views/Popular.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="view-toggle">
-      <button
-        :class="{ active: $route.name === 'PopularTable' }"
-        @click="navigateTo('PopularTable')"
-      >
-        Table View
-      </button>
-      <button
-        :class="{ active: $route.name === 'PopularInfinite' }"
-        @click="navigateTo('PopularInfinite')"
-      >
-        무한 스크롤 View
-      </button>
+    <div>
+      <div class="view-toggle">
+        <button
+          :class="{ active: $route.name === 'PopularTable' }"
+          @click="navigateTo('PopularTable')"
+        >
+          Table View
+        </button>
+        <button
+          :class="{ active: $route.name === 'PopularInfinite' }"
+          @click="navigateTo('PopularInfinite')"
+        >
+          무한 스크롤 View
+        </button>
+      </div>
+      <router-view /> <!-- 라우터에 따라 적절한 컴포넌트 렌더링 -->
     </div>
-    <router-view /> <!-- 라우터에 따라 적절한 컴포넌트 렌더링 -->
   </template>
   
   <script>

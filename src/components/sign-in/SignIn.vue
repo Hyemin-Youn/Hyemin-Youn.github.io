69f57b98fe72bb6eccfa6686d1057dab5afd96b4
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-27 23:11:27 +0900

    update signin back photo

--- src/components/sign-in/SignIn.vue
+++ src/components/sign-in/SignIn.vue
@@ -198,7 +198,7 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
-  background-image: url('https://images.unsplash.com/photo-1536440136628-849c177e76a1?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+  background-image: url('https://images.unsplash.com/photo-1512070679279-8988d32161be?q=80&w=1938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
   background-size: cover;
   background-position: center;
   z-index: -1;
